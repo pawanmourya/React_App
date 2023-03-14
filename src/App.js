5eba4436b93d4cad89c823bcab419b88646d3028
Pawan Mourya <business.pawanmourya@gmail.com>
2023-03-15 04:18:45 +0530

    main page change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing is on the wayy!
         </a>
       </header>
     </div>
